ca572d886d0d2540289fb03e0e890d53afe0cbb2
Siianitsia <pilipsia@gmail.com>
2022-09-11 18:47:56 +0300

    commit#4

--- script_159.py
+++ script_159.py
@@ -34,9 +34,8 @@ domen = 'https://exist.ua/'
 
 def get_data():
 
-    counter = 1
-    ind = 0
-
+    counter = 159
+    ind = 158
     with open('subcategories_urls.txt', 'r', encoding='utf-8') as file:
         url = file.readlines()[ind]
         req = requests.get(url=url.strip(), headers=headers)
--- script_251.py
+++ script_251.py
@@ -34,9 +34,8 @@ domen = 'https://exist.ua/'
 
 def get_data():
 
-    counter = 1
-    ind = 0
-
+    counter = 251
+    ind = 250
     with open('subcategories_urls.txt', 'r', encoding='utf-8') as file:
         url = file.readlines()[ind]
         req = requests.get(url=url.strip(), headers=headers)
